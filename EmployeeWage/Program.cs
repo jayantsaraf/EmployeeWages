854663991b6f25f446dd19ed1029a317db9e3b12
jayantsaraf <sarafjayant24@gmail.com>
2020-09-26 19:28:35 +0530

    [jayant]Add. Wage calculation function added

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,7 +6,37 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
+            ////constants
+            int EMP_RATE_PER_HOUR = 20;
+            int IS_FULL_TIME = 1;
+
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            ////Check Attendance function
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+            ////variable
+            int empHrs = 0;
+            int empWage = 0;
+
+            ////Computation of Wages
+
+            empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage = " + empWage);
         }
     }
 }
