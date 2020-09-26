b44ef05f1c5d322d7c7031112561745b9f61bc8f
jayantsaraf <sarafjayant24@gmail.com>
2020-09-26 20:03:48 +0530

    [jayant]Add. Salary within given limit calculated

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,14 +9,14 @@ namespace EmployeeWage
             ////constants
             int EMP_RATE_PER_HOUR = 20;
             int IS_FULL_TIME = 1;
+            int DAYS = 1;
 
             Console.WriteLine("Welcome to Employee Wage Computation Program!");
 
             ////Check Attendance function
             Random random = new Random();
 
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
+            
             ////program working
 
             int empCheck = random.Next(0, 2);
@@ -33,6 +33,8 @@ namespace EmployeeWage
             ////variable
             int empHrs = 0;
             int empWage = 0;
+            int totalHrs = 0;
+            int totalEmpWage = 0;
 
             ////Computation of Wages
 
@@ -45,6 +47,29 @@ namespace EmployeeWage
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wage = " + empWage);
+
+            ////Calculate Wages till total hours less than 100 and Days less than 20
+            for (DAYS = 1; DAYS <= 20; DAYS += 1)
+            {
+                while (totalHrs <= 100)
+                {
+                    
+                    empCheck = random.Next(0, 2);
+                    if (empCheck == IS_FULL_TIME)
+                    {
+                        empHrs = 8;
+                        totalHrs += empHrs;
+                    }
+                    else
+                    {
+                        empHrs = 4;
+                        totalHrs += empHrs;
+                    }
+                }
+                empWage = totalHrs * EMP_RATE_PER_HOUR * DAYS;
+                totalEmpWage += empWage;
+            }
+            Console.WriteLine("Total Employee Wage = " + totalEmpWage);
 
 
         }
