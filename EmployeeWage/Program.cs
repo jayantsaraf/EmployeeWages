e8196eda0f1acbd8fc185022d301aa220dfdf8c9
jayantsaraf <sarafjayant24@gmail.com>
2020-09-29 11:37:19 +0530

    [jayant] Refactor. Added class method to calculate wages

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,98 +2,105 @@
 
 namespace EmployeeWage
 {
+
     class Program
     {
-        static void Main(string[] args)
+        //// Constants
+        public const int NUMBER_OF_DAYS = 20;
+        public const int IS_FULL_TIME = 8;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int MAX_HRS = 100;
+
+
+        public static int CalculateWage()
         {
-            ////constants
-            int EMP_RATE_PER_HOUR = 20;
-            int IS_FULL_TIME = 1;
-            int DAYS = 1;
-
-            Console.WriteLine("Welcome to Employee Wage Computation Program!");
-
-            ////Check Attendance function
-            Random random = new Random();
-
-            ////program working
-
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = 0, empHrs = 0, totalHrs = 0, empWage = 0, totalEmpWage = 0, days = 0;
+            while (totalHrs <= MAX_HRS && days < NUMBER_OF_DAYS)
             {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
-
-            
-            ////variable
-            int empHrs = 0;
-            int empWage = 0;
-            int totalHrs = 0;
-            int totalEmpWage = 0;
-
-            ////Computation of Wages
-
-            empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 4;////Assuming part time hours = 4
-            }
-
-            //switch method to calculate part time wages
-            switch (empCheck)
-            {
-                case 0:
-                    empHrs = 4;
-                    break;
-                case 1:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage = " + empWage);
-
-            ////Calculate Wages till total hours less than 100 and Days less than 20
-            for (DAYS = 1; DAYS <= 20; DAYS += 1)
-            {
-                while (totalHrs <= 100)
+                empCheck = random.Next(0, 2);
+                if (empCheck == IS_FULL_TIME)
                 {
-                    
-                    empCheck = random.Next(0, 2);
-                    if (empCheck == IS_FULL_TIME)
-                    {
-                        empHrs = 8;
-                        totalHrs += empHrs;
-                    }
-                    else
-                    {
-                        empHrs = 4;
-                        totalHrs += empHrs;
-                    }
+                    empHrs = 8;
+                    totalHrs += empHrs;
                 }
-                empWage = totalHrs * EMP_RATE_PER_HOUR * DAYS;
-                totalEmpWage += empWage;
+                else
+                {
+                    empHrs = 4;
+                    totalHrs += empHrs;
+                }
+                days += 1;
             }
-            Console.WriteLine("Total Employee Wage = " + totalEmpWage);
-
-
-            ////Wages in a month
-            empWage = empHrs * EMP_RATE_PER_HOUR*20;
-            Console.WriteLine("Wages in a month = " + empWage);
-
-
-
+            empWage = totalHrs * EMP_RATE_PER_HOUR * DAYS;
+            totalEmpWage += empWage;
         }
+        Console.WriteLine("Total Employee Wage = " + totalEmpWage);
+            return totalEmpWage;
+        }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Employee Wage Computation Program!");
+
+        ////Check Attendance function
+        Random random = new Random();
+
+        ////program working
+
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent");
+        }
+
+
+        ////variable
+        int empHrs = 0;
+        int empWage = 0;
+        int totalHrs = 0;
+        int totalEmpWage = 0;
+
+        ////Computation of Wages
+
+        empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs = 4;////Assuming part time hours = 4
+        }
+
+        //switch method to calculate part time wages
+        switch (empCheck)
+        {
+            case 0:
+                empHrs = 4;
+                break;
+            case 1:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
+        }
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Employee wage = " + empWage);
+
+        ////Calculate Wages till total hours less than 100 and Days less than 20
+        CalculateWage();
+
+        ////Wages in a month
+        empWage = empHrs * EMP_RATE_PER_HOUR * NUMBER_OF_DAYS;
+        Console.WriteLine("Wages in a month = " + empWage);
+
+
+
     }
+}
 }
